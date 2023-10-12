a1233f140595f287a93e6b6434bc7a1513eab76c
SkanderMsalmi <msalmi.skander@esprit.tn>
2023-10-12 14:19:50 +0100

    microservices added !

---
+++ microservices/GestionCommentaires/src/test/java/com/gustofusion/gestioncommentaires/GestionCommentairesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustofusion.gestioncommentaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCommentairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
