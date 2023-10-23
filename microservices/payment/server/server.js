2f270254833ad88228841fbb85d3eb4e386b0742
SkanderMsalmi <msalmi.skander@esprit.tn>
2023-10-23 04:27:33 +0100

    fixed

--- microservices/payment/server/server.js
+++ microservices/payment/server/server.js
@@ -1,37 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { resolve } = require('path');
+const { resolve } = require("path");
 // Replace if using a different env file or config
-const env = require('dotenv').config({ path: '../.env' });
+const env = require("dotenv").config({ path: "../.env" });
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2020-08-27',
-  appInfo: { // For sample support and debugging, not required for production:
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2020-08-27",
+  appInfo: {
+    // For sample support and debugging, not required for production:
     name: "stripe-samples/payment",
     version: "0.0.1",
-    url: "https://github.com/stripe-samples"
-  }
+    url: "https://github.com/stripe-samples",
+  },
 });
 
-app.use(express.static(process.env.STATIC_DIR + '/../payment/client'));
+app.use(express.static(process.env.STATIC_DIR + "/../payment/client"));
 app.use(
   express.json({
     // We need the raw body to verify webhook signatures.
     // Let's compute it only when hitting the Stripe webhook endpoint.
-    verify: function(req, res, buf) {
-      if (req.originalUrl.startsWith('/webhook')) {
+    verify: function (req, res, buf) {
+      if (req.originalUrl.startsWith("/webhook")) {
         req.rawBody = buf.toString();
       }
-    }
+    },
   })
 );
 
-app.get('/', (req, res) => {
-  const path = resolve(process.env.STATIC_DIR + '/../payment/client'+ '/index.html');
+app.get("/", (req, res) => {
+  const path = resolve(
+    process.env.STATIC_DIR + "/../payment/client" + "/index.html"
+  );
   res.sendFile(path);
 });
 
-app.get('/config', (req, res) => {
+app.get("/config", (req, res) => {
   res.send({
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   });
@@ -40,14 +43,14 @@ app.get('/config', (req, res) => {
 // Expose a endpoint as a webhook handler for asynchronous events.
 // Configure your webhook in the stripe developer dashboard
 // https://dashboard.stripe.com/test/webhooks
-app.post('/webhook', async (req, res) => {
+app.post("/webhook", async (req, res) => {
   let data, eventType;
 
   // Check if webhook signing is configured.
   if (process.env.STRIPE_WEBHOOK_SECRET) {
     // Retrieve the event by verifying the signature using the raw body and secret.
     let event;
-    let signature = req.headers['stripe-signature'];
+    let signature = req.headers["stripe-signature"];
     try {
       event = stripe.webhooks.constructEvent(
         req.rawBody,
@@ -67,15 +70,44 @@ app.post('/webhook', async (req, res) => {
     eventType = req.body.type;
   }
 
-  if (eventType === 'payment_intent.succeeded') {
+  if (eventType === "payment_intent.succeeded") {
     // Funds have been captured
     // Fulfill any orders, e-mail receipts, etc
     // To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds)
-    console.log('💰 Payment captured!');
-  } else if (eventType === 'payment_intent.payment_failed') {
-    console.log('❌ Payment failed.');
+    console.log("💰 Payment captured!");
+  } else if (eventType === "payment_intent.payment_failed") {
+    console.log("❌ Payment failed.");
   }
   res.sendStatus(200);
 });
+const Eureka = require("eureka-js-client").Eureka;
 
-app.listen(4242, () => console.log(`Node server listening at http://localhost:4242`));
+const client = new Eureka({
+  instance: {
+    app: "gestion_payment",
+    instanceId: "/api/payment",
+    hostName: "localhost",
+    ipAddr: "127.0.0.1",
+    statusPageUrl: "http://localhost:3000",
+    port: {
+      $: 3000,
+      "@enabled": "true",
+    },
+    vipAddress: "YOUR-NODE-SERVICE",
+    dataCenterInfo: {
+      "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+      name: "MyOwn",
+    },
+  },
+  eureka: {
+    // eureka server URL
+    host: "localhost",
+    port: 8761,
+    servicePath: "../../eurekaserver",
+  },
+});
+client.start();
+
+app.listen(4242, () =>
+  console.log(`Node server listening at http://localhost:4242`)
+);
